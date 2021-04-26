80be7db4cf24806da82570d1307264a18e36162f
ChAyLom <ilya_chavlytko@mail.ru>
2021-04-26 11:51:58 +0700

    Добавьте файлы проекта.

---
+++ HolaMiAmigo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HolaMiAmigo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mi amigo");
+        }
+    }
+}
